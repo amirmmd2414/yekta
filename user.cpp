321aed02eff546cc420d29f1da07775444d30249
amir <shmir2414@gmail.com>
2024-03-18 13:25:47 +0330

    adding files

---
+++ user.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 3/18/2024.
+//
+
+#include "user.h"
---
+++ user.h
@@ -0,0 +1,8 @@
+//
+// Created by Asus on 3/18/2024.
+//
+
+#ifndef YEKTA_USER_H
+#define YEKTA_USER_H
+
+#endif //YEKTA_USER_H
